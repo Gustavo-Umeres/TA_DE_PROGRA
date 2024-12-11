fc717c9c5a5f16300e453d46e0d44ab339a54fbd
gustavo <2213010707@untels.edu.pe>
2024-12-10 19:42:21 -0500

    detalles

--- appproject/utils.py
+++
@@ -1,6 +0,0 @@
-
-def add_to_cart(request, product_id):
-    cart = request.session.get('cart', [])
-    if product_id not in cart:
-        cart.append(product_id)
-    request.session['cart'] = cart
